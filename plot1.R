9b5403fe726f04d619a2e221516155d66b4af2a5
Sreehariks <sreehariks@gmail.com>
2014-12-07 21:34:41 +0530

    R files and Image added

---
+++ plot1.R
@@ -0,0 +1,19 @@
+
+plot1 <- function(){
+     
+     plotdata <- "./data/household_power_consumption.txt"
+
+     ## read data file and filter the data for 1/2/2007 and 2/2/2007
+     
+     data <- read.table(plotdata, header=TRUE, sep=";", stringsAsFactors=FALSE, dec="."))
+     subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+     
+     ## generate a histogram and store in plot1.png
+     
+     globalActivePower <- as.numeric(subSetData$Global_active_power)
+     png("plot1.png", width=480, height=480)
+ 
+     hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+     dev.off()
+
+}
---
+++ plot3.R
@@ -0,0 +1,25 @@
+
+plotdata <- "./data/household_power_consumption.txt"
+
+# read and filter data for 1/2/2007 and 2/2/2007
+
+data <- read.table(plotdata, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+
+# Get Submetering data 
+
+subMetering1 <- as.numeric(subSetData$Sub_metering_1)
+subMetering2 <- as.numeric(subSetData$Sub_metering_2)
+subMetering3 <- as.numeric(subSetData$Sub_metering_3)
+
+# set the image size to 480 X 480
+
+png("plot3.png", width=480, height=480)
+plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+dev.off()
